872796f88a2679c54c7fe78ba07f6ead7d3334e9
isaveenstra <isaveenstra@outlook.com>
2023-11-13 16:17:06 +0100

    Added a click argument so that the person who runs the code can choose the number.

--- src/just_count/square.py
+++ src/just_count/square.py
@@ -1,6 +1,12 @@
+import click 
+
+# Voeg click toe en pas het script aan zodat je het getal zelf kan kiezen
+@click.command()
+@click.argument("number")
+
 def square(x):
     return x**2
 
 
 if __name__ == "__main__":
-    print(f"The square of 4 is {square(4)}")
+    print(f"The square of {number} is {square(number)}")
